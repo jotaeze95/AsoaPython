b1d1eb121069d5c91404d58fef488ce03bb0a9af
jotaeze95 <30812102+jotaeze95@users.noreply.github.com>
2017-11-25 01:13:46 -0300

    Calculo2

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,9 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     return "Hello World!"
-print "Pares: "
+
+for i in range(1, 1000000):
+    print(i) 
 
 if __name__ == "__main__":
     application.run()
