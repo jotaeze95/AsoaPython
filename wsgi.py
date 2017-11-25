d53c7841407bc56e309921560ffccdfe30ad745d
jotaeze95 <30812102+jotaeze95@users.noreply.github.com>
2017-11-25 01:29:13 -0300

    Calculo4

--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,18 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Contador de numeros Python"
 
-for i in range(1, 1000001):
-    return(i) 
+def contar():
+    for i in range(1, 1000001):
+        return (i)
+
+start_time = time()
+contar()
+end_time = time() - start_time
+def resul(end_time):
+    return ("Tiempo de ejecucion: %.10f seconds." % end_time)
+    
 
 if __name__ == "__main__":
     application.run()
